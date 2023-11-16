7c61851cd55d92bafc67eba928591121d8fd42d1
yves mfura <mfurayves25@gmail.com>
2023-11-16 10:49:56 -0800

    update

--- betty_check.c
+++ betty_check.c
@@ -4,21 +4,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-    const char* message = "Simple Shell Program Project";
-    const char* filepath = "./example.txt";
-    int file_descriptor = open(filepath, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-    if (file_descriptor == -1) {
-        perror("Error opening file for writing");
-        exit(EXIT_FAILURE);
-    }
+/**
+ * @opening file
+ *
+ * @return 0 on successful execution
+ */
+int main(void)
+{
+const char *message = "Simple Shell Program Project";
+const char *filepath = "./example.txt";
+int file_descriptor = open(filepath, O_WRONLY | O_CREAT | O_TRUNC, 0666);
 
-    ssize_t bytes_written = write(file_descriptor, message, strlen(message));
-    if (bytes_written == -1) {
-        perror("Error writing to file");
-        exit(EXIT_FAILURE);
-    }
-
-    close(file_descriptor);
-    return 0;
+if (file_descriptor == -1)
+{
+perror("Error opening file for writing");
+exit(EXIT_FAILURE);
 }
+
+ssize_t bytes_written = write(file_descriptor, message, strlen(message));
+
+if (bytes_written == -1)
+{
+perror("Error writing to file");
+exit(EXIT_FAILURE);
+}
+close(file_descriptor);
+
+return (0);
+}
+

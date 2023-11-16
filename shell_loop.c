35fb95ac63c23e8868f25f2cfa2207072b08c068
yvesmfura <132387277+yvesmfura@users.noreply.github.com>
2023-11-16 23:30:34 +0200

    Update shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -6,7 +6,9 @@
  * @av: the argument vector from main()
  *
  * Return: 0 on success, 1 on error, or error code
+ * Done by Yves and leonce
  */
+
 int hsh(info_t *info, char **av)
 {
 	ssize_t r = 0;
@@ -52,6 +54,7 @@ int hsh(info_t *info, char **av)
  *			1 if builtin found but not successful,
  *			-2 if builtin signals exit()
  */
+
 int find_builtin(info_t *info)
 {
 	int i, built_in_ret = -1;
